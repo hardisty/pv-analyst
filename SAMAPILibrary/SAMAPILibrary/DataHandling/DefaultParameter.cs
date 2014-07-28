d072311e6533732f86850d9a414be030af3f10d2
jar339 <jar339@psu.edu>
2014-07-28 09:39:33 -0400

    Generic approach to parameters
    Testing an approach to the parameters using generics that will allow for
    more flexible setting of defaults.

---
+++ SAMAPILibrary/SAMAPILibrary/DataHandling/DefaultParameter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SAMAPILibrary.DataHandling
+{
+    abstract class DefaultParameter<T>
+    {
+        public bool validate(Parameter<T> p);
+    }
+}
---
+++ SAMAPILibrary/SAMAPILibrary/DataHandling/Parameter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SAMAPILibrary.SAMAPI;
+
+namespace SAMAPILibrary.DataHandling
+{
+    abstract class Parameter<T>
+    {
+        public readonly String name;
+        public readonly String description;
+        readonly T value;
+
+        public Parameter(string name, string description, T value)
+        {
+            this.name = name;
+            this.description = description;
+            this.value = value;
+        }
+
+        public void setValue(Data data)
+        {
+            if (value is int || value is float)
+            {
+                float v;
+                if (value is int)
+                {
+                    int i = (int)value;
+                    v = (float)Convert.ToInt32(value);
+                }
+                else
+                {
+                    v = Convert.ToSingle(value);
+                }
+                data.SetNumber(name, v);
+            }
+            else if (value is float[])
+            {
+                data.SetArray(name, value as float[]);
+            }
+            else if (value is string || value is String)
+            {
+                data.SetString(name, value as string);
+            }
+        }
+
+
+        public T getValue()
+        {
+            return value;
+        }
+
+
+    }
+}
