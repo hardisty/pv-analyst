333dfa2ce7d85f8a0d49c1b6d226edef538c7aad
Hardisty <frank.hardisty@gmail.com>
2014-08-21 15:11:55 -0400

    developing interface

--- SAMAPILibrary/SAMAPILibrary/DataHandling/Input/GISInput.cs
+++ SAMAPILibrary/SAMAPILibrary/DataHandling/Input/GISInput.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using SAMAPILibrary.DataHandling.Parameters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace SAMAPILibrary.DataHandling.Input
 {
-    class GISInput
+
+    interface IGISInput : IEnumerable<GISData>
     {
+
     }
+
+    class GISFileInput : IGISInput
+    {
+
+    }
+
+
+
 }
